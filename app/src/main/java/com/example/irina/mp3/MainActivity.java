465b740e419f93416e1c10de50acdb206d6e9c2d
Hantinka <hantinka@gmail.com>
2016-11-17 16:33:26 +0200

    logs, pop-up messages, menu

--- app/src/main/java/com/example/irina/mp3/MainActivity.java
+++ app/src/main/java/com/example/irina/mp3/MainActivity.java
@@ -2,33 +2,38 @@ package com.example.irina.mp3;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private ImageButton buttonPlayPause;
     private boolean play = true;
-
+    private static final String TAG = "myLogs";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "Найдем View-элементы");
         buttonPlayPause = (ImageButton) findViewById(R.id.buttonPlayPause);
         buttonPlayPause.setImageResource(R.drawable.player_play_normal);
+        Log.d(TAG, "Присваиваем обработчик кнопкам");
         buttonPlayPause.setOnClickListener(onClickButtonPlayPause);
+        Toast.makeText(this, "Текст всплывающего сообщения", Toast.LENGTH_LONG).show();
     }
+
+
     View.OnClickListener onClickButtonPlayPause = new View.OnClickListener(){
     @Override
-    public void onClick(View v){
-        if (play){
+    public void onClick(View v) {
+        if (play) {
             buttonPlayPause.setImageResource(R.drawable.player_pause_normal);
-        }
-        else {
+        } else {
             buttonPlayPause.setImageResource(R.drawable.player_play_normal);
         }
         play = !play;
